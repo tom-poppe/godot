773a4f083bd3a5ff411b89f0abdbdd19315aeddd
Tom Poppe <tom@poppe.gent>
2023-11-19 10:56:22 +0100

    Registration & logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -4,14 +4,13 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -19,16 +18,16 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        $checkMail = false;
-
-        if ($request->query->has("check-mail")) {
-            $checkMail = true;
-        }
-
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'         => $error,
-            'checkMail'         => $checkMail,
         ]);
+    }
+
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        // controller can be blank: it will never be called!
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
