8a0d6c11c40b56d9e6b4b04dfb449f3b8abe5754
Tom Poppe <tom@poppe.gent>
2023-11-24 14:35:50 +0100

    Voter for security

--- src/Controller/NoteController.php
+++ src/Controller/NoteController.php
@@ -10,10 +10,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/note')]
 class NoteController extends AbstractController
 {
+    #[Route('/', name: 'app_note_index', methods: ['GET'])]
+    public function index(NoteRepository $noteRepository, Request $request): Response
+    {
+        $noteId = $request->query->get('n');
+
+        return $this->render('note/index.html.twig', [
+            'notes' => $noteRepository->findBy(["user" => $this->getUser()], ["updatedAt" => "DESC"]),
+            'selected' => $noteId,
+        ]);
+    }
+
     #[Route('/new', name: 'app_note_new', methods: ['GET'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
@@ -31,12 +43,19 @@ class NoteController extends AbstractController
         return new Response($note->getId());
     }
 
+    #[Route('/{id}/show', name: 'app_note_show', methods: ['GET'])]
+    #[IsGranted('access', 'note')]
+    public function show(Note $note): Response
+    {
+        return $this->render('note/show.html.twig', [
+            'note' => $note,
+        ]);
+    }
+
     #[Route('/{id}/edit', name: 'app_note_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('access', 'note')]
     public function edit(Request $request, Note $note, EntityManagerInterface $entityManager): Response
     {
-        # Check: maybe in a voter?
-        $this->checkAccess($note->getUser());
-
         $form = $this->createForm(NoteType::class, $note);
         $form->handleRequest($request);
 
@@ -54,10 +73,9 @@ class NoteController extends AbstractController
     }
 
     #[Route('/{id}/delete', name: 'app_note_delete', methods: ['GET'])]
+    #[IsGranted('access', 'note')]
     public function delete(Request $request, Note $note, EntityManagerInterface $entityManager): Response
     {
-        $this->checkAccess($note->getUser());
-
         $note->setUpdatedAt(new \DateTime('now'));
         $note->setDeletedAt(new \DateTime('now'));
 
@@ -65,33 +83,5 @@ class NoteController extends AbstractController
         $entityManager->flush();
 
         return new Response("Note deleted");
-    }
-
-    #[Route('/{id}', name: 'app_note_show', methods: ['GET'])]
-    public function show(Note $note): Response
-    {
-        $this->checkAccess($note->getUser());
-
-        return $this->render('note/show.html.twig', [
-            'note' => $note,
-        ]);
-    }
-
-    #[Route('/', name: 'app_note_index', methods: ['GET'])]
-    public function index(NoteRepository $noteRepository, Request $request): Response
-    {
-        $noteId = $request->query->get('n');
-
-        return $this->render('note/index.html.twig', [
-            'notes' => $noteRepository->findBy(["user" => $this->getUser()], ["updatedAt" => "DESC"]),
-            'selected' => $noteId,
-        ]);
-    }
-
-    private function checkAccess($user): void
-    {
-        if ($user !== $this->getUser()) {
-            throw $this->createAccessDeniedException();
-        }
     }
 }
